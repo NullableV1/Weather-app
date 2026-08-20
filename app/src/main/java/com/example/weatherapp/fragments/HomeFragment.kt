8df291cd652c3223bc7e5b23f7a00ae79fb2d353
ayoub <toulnasro27@gmail.com>
2026-08-20 02:08:00 +0100

    Cleaning HomeFragment.kt

--- app/src/main/java/com/example/weatherapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/HomeFragment.kt
@@ -15,17 +15,6 @@ import com.example.weatherapp.local.PreferencesManager
 import com.example.weatherapp.viewmodels.WeatherViewModel
 import com.google.android.material.snackbar.Snackbar
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
